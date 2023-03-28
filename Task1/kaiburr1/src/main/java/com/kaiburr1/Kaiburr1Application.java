ba3606773c03983c800ab9cfdc6f78ab728ca4d2
prs2002 <64260524+prs2002@users.noreply.github.com>
2023-03-29 03:45:19 +0530

    Add files via upload

---
+++ Task1/kaiburr1/src/main/java/com/kaiburr1/Kaiburr1Application.java
@@ -0,0 +1,13 @@
+package com.kaiburr1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kaiburr1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kaiburr1Application.class, args);
+	}
+
+}
---
+++ Task1/kaiburr1/src/test/java/com/kaiburr1/Kaiburr1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaiburr1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kaiburr1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
